c1bc3f87a1c12b484a17840e180babf026d121cd
Alex Levenson <alexlevenson@laptop.org>
2008-07-10 16:55:19 +0000

    Fixed joint tool right click

--- tools.py
+++ tools.py
@@ -274,8 +274,8 @@ class JointTool(Tool):
                     # regardless, clean everything up
                     self.jb1 = self.jb2 = self.jb1pos = self.jb2pos = None
                 if event.button == 3:
-                    # add a centered fixed joint
-                    self.jb2 = world.get_bodies_at_pos(event.pos)
+                    # add a centered fisxed joint
+                    self.jb2 = self.game.world.get_bodies_at_pos(event.pos)
                     if self.jb2:
                         self.game.world.add.joint(self.jb2[0])
     def draw(self):
