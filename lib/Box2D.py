bf4640fd7bd5f29be6afec288ae326b3f5398a97
James Cameron <quozl@laptop.org>
2017-06-07 14:32:50 +1000

    Fix for Fedora 18 32-bit fail to start #4926
    On a Fedora 18 32-bit VM, installed swig 3.0.10 from sources into
    /usr/local, removed the system swig, cloned pybox2d 2.3.2 055428c, then
    did `python setup.py build` and copied the `build/lib.linux-i686-2.7`
    tree to Physics.
    
    Fixes https://bugs.sugarlabs.org/ticket/4926
    
    Signed-off-by: James Cameron <quozl@laptop.org>

--- lib/Box2D.py
+++ lib/Box2D.py
@@ -6,9 +6,4 @@ else:
     if platform.architecture()[0] == '64bit':
         from box2d_64 import *
     else:
-        from ctypes import cdll
-        from sys import path
-        path.insert(0, 'lib/box2d_32')
-        libstdc = cdll.LoadLibrary("lib/box2d_32/libstdc++.so")
-        libstdc = cdll.LoadLibrary("lib/box2d_32/libstdc++.so.6")
         from box2d_32 import *
