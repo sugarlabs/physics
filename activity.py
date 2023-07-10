sugarlabs/physics

commit f82ca550f392cc514d7bbb27fc05521464e1c808

Fix video system not initialised error
- two code paths were calling pygame.quit

- change to follow sugargame 1.2 style.

activity.py
@@ -31,7 +31,6 @@ import gi
 gi.require_version('Gtk', '3.0')
 
 import pygame
-import sugargame
 import sugargame.canvas
 
 from gi.repository import GObject
@@ -60,7 +59,7 @@ except ImportError:
     from collabwrapper import CollabWrapper
 
 import tools
-import physics
+from physics import PhysicsGame
 
 # For some reason increasing FPS decreases execution speed :/
 SLOWEST_FPS = 90
@@ -81,7 +80,7 @@ class PhysicsActivity(activity.Activity):
         self.connect('visibility-notify-event', self._focus_event)
         self.connect('window-state-event', self._window_event)
 
-        self.game = physics.main(self)
+        self.game = PhysicsGame(self)
         self.game.canvas = sugargame.canvas.PygameCanvas(
             self, main=self.game.run, modules=[pygame.display, pygame.font])
 
@@ -116,7 +115,6 @@ class PhysicsActivity(activity.Activity):
                                 pygame.RESIZABLE)
         self.read_file(os.path.join(
                        activity.get_activity_root(), 'data', 'data'))
-        self.game.run(True)
 
     def read_file(self, file_path):
         self.game.read_file(file_path)
@@ -276,7 +274,7 @@ class PhysicsActivity(activity.Activity):
 
     def can_close(self):
         self.preview = self.get_preview()
-        self.game.loop = False
+        self.game.running = False
         return True
 
     def _insert_stop_play_button(self, toolbar):
@@ -467,9 +465,6 @@ class PhysicsActivity(activity.Activity):
     def _focus_event(self, event, data=None):
         ''' Send focus events to pygame to allow it to idle when in
         background. '''
-        if not self.game.pygame_started:
-            logging.debug('focus_event: pygame not yet initialized')
-            return
         if data.state == Gdk.VisibilityState.FULLY_OBSCURED:
             pygame.event.post(pygame.event.Event(pygame.USEREVENT,
                                                  action='focus_out'))
@@ -642,7 +637,6 @@ class PhysicsActivity(activity.Activity):
                 f.close()
 
                 self.read_file(file_path)
-                self.game.run(True)
             except:
                 title = _('Load project from journal')
                 if not journal:

physics.py
@@ -54,8 +54,8 @@ class PhysicsGame:
         # Set up the world (instance of Elements)
         self.box2d = box2d
         self.opening_queue = None
-        self.loop = True
-        self.pygame_started = False
+        self.running = True
+        self.initialise = True
 
         self.full_pos_list = []
         self.tracked_bodies = 0
@@ -87,11 +87,9 @@ class PhysicsGame:
         # Loading from journal
         self.opening_queue = path
 
-    def run(self, restart=False):
-        self.screen = pygame.display.get_surface()
-        if not restart:
-            pygame.display.init()
-            self.pygame_started = True
+    def run(self):
+        if self.initialise:
+            self.initialise = False
 
             # Fake a Sugar cursor for the pyGame canvas area
             self.show_fake_cursor = True
@@ -106,6 +104,7 @@ class PhysicsGame:
             self.canvas.add_events(Gdk.EventMask.ENTER_NOTIFY_MASK |
                                    Gdk.EventMask.LEAVE_NOTIFY_MASK)
 
+        self.screen = pygame.display.get_surface()
         self.world = elements.Elements(self.screen.get_size())
         self.world.renderer.set_surface(self.screen)
         self.world.add.ground()
@@ -123,17 +122,23 @@ class PhysicsGame:
                     self.tracked_bodies = \
                         self.world.additional_vars['tracked_bodies']
 
-        while self.loop:
+        while self.running:
+
+            # Pump GTK messages.
             while Gtk.events_pending():
                 Gtk.main_iteration()
-
-            if not self.loop:
-                pygame.quit()
-                self.pygame_started = False
+            if not self.running:
                 break
 
+            # Pump PyGame messages.
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.VIDEORESIZE:
+                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
+
                 self.currentTool.handleEvents(event)
+
                 if event.type == MOUSEBUTTONUP:
                     # if event.button == 1:
                     self.show_fake_cursor = True
@@ -215,6 +220,8 @@ class PhysicsGame:
             # Stay < 30 FPS to help keep the rest of the platform responsive
             self.clock.tick(30)  # Originally 50
 
+        return False
+
     def setTool(self, tool):
         self.currentTool.cancel()
         self.currentTool = self.toolList[tool]
@@ -222,13 +229,3 @@ class PhysicsGame:
 
     def get_activity(self):
         return self.activity
-
-
-def main(activity):
-    game = PhysicsGame(activity)
-    return game
-
-
-# Make sure that main get's called
-if __name__ == '__main__':
-    main()

commit bc278702f5af7b25c94398e9c1feecaf1cd6743c

Fix: Sample fails to load
Closes #49
Regression introduced in f82ca550f392cc514d7bbb27fc05521464e1c808

activity.py
@@ -646,6 +646,7 @@ class PhysicsActivity(Activity):
                 f.close()
 
                 self.read_file(file_path)
+                self.game.check_queue()
             except:
                 title = _('Load project from journal')
                 if not journal:

physics.py
@@ -87,6 +87,20 @@ class PhysicsGame:
         # Loading from journal
         self.opening_queue = path
 
+    def check_queue(self):
+        if self.opening_queue:
+            path = self.opening_queue.encode('ascii', 'convert')
+            if os.path.exists(path):
+                self.world.json_load(path, serialized=True)
+                if 'full_pos_list' in self.world.additional_vars:
+                    self.full_pos_list = \
+                        self.world.additional_vars['full_pos_list']
+                if 'trackinfo' in self.world.additional_vars:
+                    self.trackinfo = self.world.additional_vars['trackinfo']
+                if 'tracked_bodies' in self.world.additional_vars:
+                    self.tracked_bodies = \
+                        self.world.additional_vars['tracked_bodies']
+
     def run(self):
         if self.initialise:
             self.initialise = False
@@ -108,19 +122,8 @@ class PhysicsGame:
         self.world = elements.Elements(self.screen.get_size())
         self.world.renderer.set_surface(self.screen)
         self.world.add.ground()
+        self.check_queue()
 
-        if self.opening_queue:
-            path = self.opening_queue.encode('ascii', 'convert')
-            if os.path.exists(path):
-                self.world.json_load(path, serialized=True)
-                if 'full_pos_list' in self.world.additional_vars:
-                    self.full_pos_list = \
-                        self.world.additional_vars['full_pos_list']
-                if 'trackinfo' in self.world.additional_vars:
-                    self.trackinfo = self.world.additional_vars['trackinfo']
-                if 'tracked_bodies' in self.world.additional_vars:
-                    self.tracked_bodies = \
-                        self.world.additional_vars['tracked_bodies']
 
         while self.running:
 